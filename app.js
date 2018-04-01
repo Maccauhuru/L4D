2b13eef7a9a6bd224e256736d612563d5a3fb283
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-03-31 19:06:21 -0500

    add about route

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express           = require('express');
 const app               = express();
-const port              = 3000 || process.env.PORT;
+const port              = 3050 || process.env.PORT;
 
 //INDEX ROUTE
 app.get('/',(req,res) =>res.send("Home Page"));
+
+// ABOUT ROUTE
+app.get('/about',(req,res)=>res.send("About Page"));
 
 app.listen(port,()=>console.log(`listening to port: ${port}`));
 
